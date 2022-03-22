8510e526e409cbbf8f09f1c27311cf2c9421087f
GuanWei <s1092790@gm.pu.edu.tw>
2022-03-22 09:49:13 +0800

    Second commit(修改文字文件)

--- app/src/main/java/tw/edu/pu/gm/hello2022gw/MainActivity.kt
+++ app/src/main/java/tw/edu/pu/gm/hello2022gw/MainActivity.kt
@@ -2,6 +2,7 @@ package tw.edu.pu.gm.hello2022gw
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -9,7 +10,6 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
     }
 
-    var txv:TextView =
-        findViewById<TextView>(R.id.txv)
+    var txv:TextView = findViewById<TextView>(R.id.txv)
     txv.text = "Hello gw"
 }
